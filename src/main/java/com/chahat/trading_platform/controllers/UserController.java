54e471ee4f8ad3f7e29335d0b7563a267b3bfd0d
chahats2002@gmail.com <chahats2002@gmail.com>
2025-01-15 02:05:23 +0530

    TwoFactor Auth added

--- src/main/java/com/chahat/trading_platform/controllers/UserController.java
+++ src/main/java/com/chahat/trading_platform/controllers/UserController.java
@@ -4,9 +4,9 @@ import com.chahat.trading_platform.model.User;
 import com.chahat.trading_platform.service.EmailService;
 import com.chahat.trading_platform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -17,5 +17,15 @@ public class UserController {
     @Autowired
     private EmailService emailService;
 
-    public ResponseEntity<User> getUserProfile(@RequestHeader )
+    @GetMapping("api/users/profile")
+    public ResponseEntity<User> getUserProfile(@RequestHeader("Authorization") String jwt ) throws Exception {
+        User user = userService.findUserByJWT(jwt);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PatchMapping("api/users/enable-two-factor/verify/{otp}")
+    public ResponseEntity<User> enableTwoFactorAuth(@RequestHeader("Authorization") String jwt, @PathVariable String otp) throws Exception {
+        User user = userService.findUserByJWT(jwt);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }

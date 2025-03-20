59a16d2d7c24bd8f935ae73d54bc7609156dd485
chahats2002@gmail.com <chahats2002@gmail.com>
2025-03-21 02:23:06 +0530

    Coin entity added

--- src/main/java/com/chahat/trading_platform/controllers/CoinController.java
+++ src/main/java/com/chahat/trading_platform/controllers/CoinController.java
@@ -1,10 +1,15 @@
 package com.chahat.trading_platform.controllers;
 
+import com.chahat.trading_platform.service.CoinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/coins")
 public class CoinController {
+
+    @Autowired
+    private CoinService coinService;
 
 }
